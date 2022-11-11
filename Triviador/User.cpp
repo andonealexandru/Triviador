5c882ef0aa12020626732a4ae9983a402ba25304
andonealexandru <andonealexandru19@yahoo.com>
2022-11-11 09:47:48 +0200

    Added User and UserStatistics classes. - classes used for mapping database entities - no SmartPointers used

---
+++ Triviador/User.cpp
@@ -0,0 +1,42 @@
+#include "User.h"
+
+User::User(std::string name) : mName(std::move(name))
+{}
+
+User::User(uint8_t id, std::string name) : mId(id), mName(std::move(name))
+{}
+
+User::User(const User& other)
+{
+    mId = other.mId;
+    mName = other.mName;
+}
+
+User& User::operator=(const User& other)
+{
+    if (this != &other)
+    {
+        mId = other.mId;
+        mName = other.mName;
+    }
+}
+
+uint8_t User::GetId()
+{
+    return mId;
+}
+
+std::string User::GetName()
+{
+    return mName;
+}
+
+void User::SetId(uint8_t id)
+{
+    mId = id;
+}
+
+void User::SetName(std::string name)
+{
+    mName = std::move(name);
+}
