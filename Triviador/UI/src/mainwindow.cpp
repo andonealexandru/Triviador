e22b37b5b6be00c99575121ed531178c13b2ce00
Ramona-unitbv <ramona.bajan@student.unitbv.ro>
2022-11-27 18:22:51 +0200

    Added regions and points on map.

--- Triviador/UI/src/mainwindow.cpp
+++ Triviador/UI/src/mainwindow.cpp
@@ -68,6 +68,76 @@ void MainWindow::paintEvent(QPaintEvent* pe)
     p.setFont(QFont("Arial", widWidth * widHeight / 55666));
     p.drawText(r, Qt::AlignCenter, s);
 
+    r.setRect(widWidth / 1.83, widHeight / 5.81, widWidth / 21, widHeight / 11.5);
+    p.setBrush(blue);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
+
+
+    px.load("F:/AN_2/Regions_pics/Cultura_green.png");
+    px = px.scaled(widWidth / 5.8, widHeight / 5.8, Qt::IgnoreAspectRatio);
+    pt.drawPixmap(widWidth / 1.85, widHeight / 4.3, px);
+    QWidget::paintEvent(pe);
+
+    r.setRect(widWidth / 1.7, widHeight / 3.44, widWidth / 21, widHeight / 11.5);
+    p.setBrush(green);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
+
+
+    px.load("F:/AN_2/Regions_pics/Cheile_Bicazului_green.png");
+    px = px.scaled(widWidth / 5.45, widHeight / 3.35, Qt::IgnoreAspectRatio);
+    pt.drawPixmap(widWidth / 2.38, widHeight / 3.3, px);
+    QWidget::paintEvent(pe);
+
+    r.setRect(widWidth / 2.08, widHeight / 2.632, widWidth / 21, widHeight / 11.5);
+    p.setBrush(green);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
+
+
+    px.load("F:/AN_2/Regions_pics/Trandafiri_red.png");
+    px = px.scaled(widWidth / 6.4, widHeight / 5.7, Qt::IgnoreAspectRatio);
+    pt.drawPixmap(widWidth / 1.761, widHeight / 2.9, px);
+    QWidget::paintEvent(pe);
+
+    r.setRect(widWidth / 1.615, widHeight / 2.52, widWidth / 21, widHeight / 11.5);
+    p.setBrush(red);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
+
+    px.load("F:/AN_2/Regions_pics/Evreii_red.png");
+    px = px.scaled(widWidth / 7.5, widHeight / 7, Qt::IgnoreAspectRatio);
+    pt.drawPixmap(widWidth / 1.685, widHeight / 2.035, px);
+    QWidget::paintEvent(pe);
+
+    r.setRect(widWidth / 1.579, widHeight / 1.935, widWidth / 21, widHeight / 11.5);
+    p.setBrush(red);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
+
+
+    px.load("F:/AN_2/Regions_pics/Teatru_green.png");
+    px = px.scaled(widWidth / 5.4, widHeight / 2.43, Qt::IgnoreAspectRatio);
+    pt.drawPixmap(widWidth / 6.98, widHeight / 2.556, px);
+    QWidget::paintEvent(pe);
+
+    r.setRect(widWidth / 4.29, widHeight / 1.9, widWidth / 21, widHeight / 11.5);
+    p.setBrush(green);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
 }
 
 MainWindow::~MainWindow()
