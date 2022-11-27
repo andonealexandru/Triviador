d75b843425fb2f890e64d16c14488e2556e9227c
Andreea-unitbv <andreea.musca@student.unitbv.ro>
2022-11-27 15:39:50 +0200

    Created a login window connected with the start window.

---
+++ Triviador/UI/src/Login.cpp
@@ -0,0 +1,19 @@
+#include "Login.h"
+#include "ui_Login.h"
+
+Login::Login(QWidget* parent) :
+    QDialog(parent),
+    ui(new Ui::Login)
+{
+    ui->setupUi(this);
+}
+
+Login::~Login()
+{
+    delete ui;
+}
+
+void Login::on_pushButton_clicked()
+{
+    emit pushButtonPressed();
+}
