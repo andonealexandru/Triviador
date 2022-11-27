4b667efb4eb33b7eba9355183480777d8e847dc2
WallachianMihai <mihaimunteanu000@gmail.com>
2022-11-27 20:26:48 +0200

    Remove unique_ptr mapping reference to User table

--- Triviador/Database/include/UserStatisticsService.h
+++ Triviador/Database/include/UserStatisticsService.h
@@ -9,7 +9,7 @@ namespace DB
         // constructors
         UserStatisticsService() = default;
         UserStatisticsService(const uint8_t userId, const uint8_t score, const uint8_t territoryCount);
-        UserStatisticsService(uint8_t id, const uint8_t userId, uint8_t score, uint8_t territoryCount);
+        UserStatisticsService(const uint8_t id, const uint8_t userId, const uint8_t score, const uint8_t territoryCount);
         UserStatisticsService(const UserStatisticsService &other) = default;
         UserStatisticsService &operator=(const UserStatisticsService &other) = default;
 
@@ -23,13 +23,13 @@ namespace DB
 
         // setters
         void SetId(uint8_t id);
-        void SetUser(uint8_t* user);
+        void SetUser(uint8_t user);
         void SetScore(uint8_t score);
         void SetTerritoryCount(uint8_t territoryCount);
 
     public:
         uint8_t m_id;
-        std::unique_ptr<uint8_t> m_userId;
+        uint8_t m_userId;
         uint8_t m_score;
         uint8_t m_territoryCount;
     };
