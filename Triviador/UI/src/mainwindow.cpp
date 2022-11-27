f924499bab501e6f2e876aba07591c3b0460f43b
Ramona-unitbv <ramona.bajan@student.unitbv.ro>
2022-11-27 18:17:59 +0200

    Added regions and points on map.

--- Triviador/UI/src/mainwindow.cpp
+++ Triviador/UI/src/mainwindow.cpp
@@ -42,6 +42,32 @@ void MainWindow::paintEvent(QPaintEvent* pe)
     p.setFont(QFont("Arial", widWidth * widHeight / 55666));
     p.drawText(r, Qt::AlignCenter, s);
 
+
+    px.load("F:/AN_2/Regions_pics/Atos_red.png");
+    px = px.scaled(widWidth / 4.75, widHeight / 4.1, Qt::IgnoreAspectRatio);
+    pt.drawPixmap(widWidth / 3.45, widHeight / 7.58, px);
+    QWidget::paintEvent(pe);
+
+    r.setRect(widWidth / 2.585, widHeight / 5.9, widWidth / 21, widHeight / 11.5);
+    p.setBrush(red);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
+
+
+    px.load("F:/AN_2/Regions_pics/Bucovina_blue.png");
+    px = px.scaled(widWidth / 5.8, widHeight / 4.6, Qt::IgnoreAspectRatio);
+    pt.drawPixmap(widWidth / 2.03, widHeight / 9.3, px);
+    QWidget::paintEvent(pe);
+
+    r.setRect(widWidth / 1.83, widHeight / 5.81, widWidth / 21, widHeight / 11.5);
+    p.setBrush(blue);
+    p.drawEllipse(r);
+    s = QString::number(100);
+    p.setFont(QFont("Arial", widWidth * widHeight / 55666));
+    p.drawText(r, Qt::AlignCenter, s);
+
 }
 
 MainWindow::~MainWindow()
