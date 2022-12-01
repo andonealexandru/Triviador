124b01bbd3200cc81513af3928f4b57bed62b234
WallachianMihai <mihaimunteanu000@gmail.com>
2022-12-01 15:19:53 +0200

    Change structure, refactor and add 2 new methods for incremeting the game counters

--- Triviador/Database/include/UserStatistics.h
+++ Triviador/Database/include/UserStatistics.h
@@ -10,28 +10,31 @@ namespace DB
     public:
         // constructors
         UserStatistics() = default;
-        UserStatistics(uint32_t* userId, const uint32_t score, const uint32_t territoryCount);
-        UserStatistics(const uint32_t id, uint32_t* userId, const uint32_t score, const uint32_t territoryCount);
+        UserStatistics(const uint32_t userId, const uint32_t score, const uint32_t territoryCount);
+        UserStatistics(const uint32_t id, const uint32_t userId, const uint32_t score, const uint32_t territoryCount);
         UserStatistics(const UserStatistics& other) = default;
-        UserStatistics&operator=(const UserStatistics& other) = default;
+        UserStatistics& operator=(const UserStatistics& other) = default;
 
         ~UserStatistics() = default;
 
         // getters
         uint32_t GetId() const;
-        uint32_t* GetUserId() const;
+        uint32_t GetUserId() const;
         uint32_t GetGameCount() const;
         uint32_t GetWonGames() const;
 
         // setters
-        void SetId(uint32_t id);
-        void SetUser(uint32_t* user);
+        void SetId(const uint32_t id);
+        void SetUser(const uint32_t user);
         void SetGameCount(const uint32_t gameCount);
         void SetWonGames(const uint32_t wonGames);
 
+        void AddGame();
+        void AddWin();
+
     private:
         uint32_t m_id;
-        std::unique_ptr<uint32_t> m_userId;
+        uint32_t m_userId;
         uint32_t m_gameCount;
         uint32_t m_wonGames;
     };
--- Triviador/Database/src/UserStatistics.cpp
+++ Triviador/Database/src/UserStatistics.cpp
@@ -2,23 +2,30 @@
 
 namespace DB
 {
-    UserStatistics::UserStatistics(uint32_t* userId, const uint32_t gameCount, const uint32_t wonGames)
+    UserStatistics::UserStatistics(const uint32_t userId, const uint32_t gameCount, const uint32_t wonGames)
             : m_userId(userId), m_gameCount(gameCount), m_wonGames(wonGames)
     {
     }
 
-    UserStatistics::UserStatistics(const uint32_t id, uint32_t* userId, const uint32_t gameCount, const uint32_t wonGames)
+    UserStatistics::UserStatistics(const uint32_t id, const uint32_t userId, const uint32_t gameCount, const uint32_t wonGames)
             : m_id(id), m_userId(userId), m_gameCount(gameCount), m_wonGames(wonGames)
     {
     }
 
     uint32_t UserStatistics::GetId() const{ return m_id; }
-    uint32_t* UserStatistics::GetUserId() const { return m_userId.get(); }
+    uint32_t UserStatistics::GetUserId() const { return m_userId; }
     uint32_t UserStatistics::GetGameCount() const { return  m_gameCount; }
     uint32_t UserStatistics::GetWonGames() const { return m_wonGames; }
 
     void UserStatistics::SetId(const uint32_t id) { m_id = id; }
-    void UserStatistics::SetUser(uint32_t* userId) { m_userId.reset(userId); }
+    void UserStatistics::SetUser(const uint32_t userId) { m_userId = userId; }
     void UserStatistics::SetGameCount(const uint32_t gameCount) { m_gameCount = gameCount; }
     void UserStatistics::SetWonGames(const uint32_t wonGames) { m_wonGames = wonGames; }
+
+    void UserStatistics::AddGame() { ++m_gameCount; }
+    void UserStatistics::AddWin()
+    {
+        ++m_gameCount;
+        ++m_wonGames;
+    }
 }//namespace DB
