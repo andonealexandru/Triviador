fd44f7abbac94877a9dd8faf06e08b2ced9d464f
WallachianMihai <mihaimunteanu000@gmail.com>
2022-12-01 15:22:21 +0200

    clean triviador files and add .sqlite to .gitignore

--- Triviador/Triviador.cpp
+++ Triviador/Triviador.cpp
@@ -1,12 +1,7 @@
-#include "Triviador.h"
-#include <iostream>
-#include <crow.h>
-#include "DBAccess.h"
+#include <Triviador.h>
+
 int main()
 {
-    auto storage = DB::DBAccess::GetInstance()->GetStorage();
-    std::cout << storage.count<DB::Question>();
-
     return 0;
 }
 
