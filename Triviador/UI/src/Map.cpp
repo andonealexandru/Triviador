b8be5ede3e025863ac95322a72fa2f54ce987676
Ramona-unitbv <ramona.bajan@student.unitbv.ro>
2022-12-18 18:50:54 +0200

    Draw the central tower of the castle.

--- Triviador/UI/src/Map.cpp
+++ Triviador/UI/src/Map.cpp
@@ -6,7 +6,7 @@
 #include <QQueue>
 
 
-Map::Map(QWidget *parent)
+Map::Map(QWidget* parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
@@ -15,69 +15,15 @@ Map::Map(QWidget *parent)
 Map::~Map()
 {}
 
-//void Map::fill(const QPoint& point, QColor act, QPainter &painter)
-//{
-//	//QPainter painter(&image);
-//	QPen myPen = QPen();
-//	myPen.setWidth(1);
-//	painter.setPen(myPen);
-//
-//	QQueue<QPoint> pixels;
-//	pixels.enqueue(point);
-//	while (pixels.isEmpty() == 0)
-//	{
-//		QPoint newPoint = pixels.dequeue();
-//		QColor actual;
-//		actual.fromRgb(QColor::rgb(newPoint));
-//		if (actual == act)
-//		{
-//			painter.drawPoint(newPoint);
-//			update();
-//
-//			QPoint left((newPoint.x() - 1), newPoint.y());
-//			if (left.x() > 0 && left.x() < image.width() && image.pixel(left) == act.rgb())
-//			{
-//				pixels.enqueue(left);
-//				painter.drawPoint(left);
-//				update();
-//			}
-//
-//			QPoint right((newPoint.x() + 1), newPoint.y());
-//			if (right.x() > 0 && right.x() < image.width() && image.pixel(right) == act.rgb())
-//			{
-//				pixels.enqueue(right);
-//				painter.drawPoint(right);
-//				update();
-//			}
-//
-//			QPoint up((newPoint.x()), (newPoint.y() - 1));
-//			if (up.y() > 0 && up.y() < image.height() && image.pixel(up) == act.rgb())
-//			{
-//				pixels.enqueue(up);
-//				painter.drawPoint(up);
-//				update();
-//			}
-//
-//			QPoint down((newPoint.x()), (newPoint.y() + 1));
-//			if (down.y() > 0 && down.y() < image.height() && image.pixel(down) == act.rgb())
-//			{
-//				pixels.enqueue(down);
-//				painter.drawPoint(down);
-//				update();
-//			}
-//		}
-//	}
-//}
-
 void Map::paintEvent(QPaintEvent* event)
 {
 
 	printf("Function called!\n");
 	QPainter painter(this);
-	painter.setPen(/*QColor(0, 0, 0)*/Qt::black);
+	painter.setPen(Qt::black);
 
-	auto nLongitudinalSectors = 50;
-	auto nTransveralSectors = 50;
+	auto nLongitudinalSectors = 100;
+	auto nTransveralSectors = 100;
 	auto width = this->size().width();
 	auto height = this->size().height();
 	auto sectorWidth = width / nLongitudinalSectors;
@@ -98,37 +44,30 @@ void Map::paintEvent(QPaintEvent* event)
 	QPixmap pixmap(sectorWidth, sectorHeight);
 	pixmap.fill(Qt::yellow);
 	painter.drawPixmap(0, 0, pixmap);
-	
-	//std::vector<QColor> colors = {QColor(255, 255, 255), QColor(255, 0, 0), QColor(0, 255, 0), QColor(0, 0, 255), QColor(255, 255, 0)};
-	///*painter.setPen(colors[2]);
-	//painter.drawPoint(50, 50);*/
 
-	//Game uselessGameInstance = Game();
-	//
-	//uselessGameInstance.ReadMap();
-	//uselessGameInstance.PrintMap();
-	//uselessGameInstance.PrintTiles();
-	//auto regions = uselessGameInstance.getMap(); // A list of pointers to regions.
-	//auto pixelsSets = uselessGameInstance.getTiles(); // A list of pixel sets coresponding to each region.
+	int x = 37;
+	int y = 37;
 
-	//// Iterate through the whole list of sets.
-	//for (int i = 0; i < pixelsSets.size(); i++)
-	//{
-	//	auto setOfPixels = pixelsSets[i];
+	//turn centru
+	QColor gray(87, 96, 100), window(38, 175, 230), back(134, 132, 130);
+	pixmap.fill(gray);
+	for (int i = y - 3;i <= y + 3;i++)
+		for (int j = x - 1;j <= x + 1;j++)
+			painter.drawPixmap(j * sectorWidth, i * sectorHeight, pixmap);
 
-	//	// Iterate through each pixel and draw it to the screen.
-	//	for (int j = 0; j < setOfPixels.size(); j++)
-	//	{
-	//		auto parentRegion = setOfPixels[j].getParentRegion();
-	//		auto coords = setOfPixels[j].GetCoordinate();
-	//		printf("Painting pixel at %d %d, of color %d\n", coords.first, coords.second, parentRegion->GetColor());
-	//		auto color = colors[parentRegion->GetColor()];
+	pixmap.fill(window);
+	for (int i = -2;i < 2;i++)
+		painter.drawPixmap(y * sectorWidth, (x + i) * sectorHeight, pixmap);
 
-	//		fflush(stdout);
-	//		painter.setPen(color);
-	//		painter.drawPoint(coords.first, coords.second);
-	//	}
-	//}
+	pixmap.fill(gray);
+	for (int i = 4;i <= 5;i++)
+	{
+		for (int j = x - 2;j <= x + 2;j++)
+			painter.drawPixmap(j * sectorWidth, (y - i) * sectorHeight, pixmap);
+	}
+
+	for (int j = x - 1;j <= x + 1;j++)
+		painter.drawPixmap(j * sectorWidth, (y - 6) * sectorHeight, pixmap);
 }
 
 
