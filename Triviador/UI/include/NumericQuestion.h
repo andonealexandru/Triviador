ce40c82c7a34008089710b374c58f3a2aeec52c0
Ramona-unitbv <ramona.bajan@student.unitbv.ro>
2023-01-06 21:05:33 +0200

    Improved NumericQuestion class with help buttons

--- Triviador/UI/include/NumericQuestion.h
+++ Triviador/UI/include/NumericQuestion.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <QMainWindow>
 #include <QTimer>
 #include "ui_NumericQuestion.h"
@@ -9,11 +8,18 @@ class NumericQuestion : public QMainWindow
 	Q_OBJECT
 
 public:
-	NumericQuestion(QWidget *parent = nullptr);
+	NumericQuestion(int correctAnswer, std::string question, bool ho1, bool ho2, bool ho3, QWidget* parent = nullptr);
 	void paintEvent(QPaintEvent* pe = nullptr) override;
 	~NumericQuestion();
-	void timer();
+	int timer();
+
+private slots:
+	void ho3Clicked();
+	void ho2Clicked();
 
 private:
 	Ui::NumericQuestionClass ui;
+	int m_correctAnswer;
+	std::string m_question;
+	bool m_ho1, m_ho2, m_ho3; //variante ajutatoare
 };
